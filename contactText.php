0f88cfd5bcac184dc7268ca7bf4924e56272d2ef
Siddharth <siddharth@ubuntu.ubuntu-domain>
2014-04-12 15:38:37 +0530

    changed the name of the GSecs on the contact page. Have mailed Arpit and Manish regarding the photo that they want on the contacts page

--- contactText.php
+++ contactText.php
@@ -11,30 +11,37 @@
 	<div class="name_left"><b>Prof. Siddhartha Mukhopadhyay,(EE) Dean, Alumni Affairs & International Relations,  </b></div><br>
 	<div class="name_left"><b>Email:</b> <a href="mailto:smukh@ee.iitkgp.ernet.in" target="a_blank">smukh@ee.iitkgp.ernet.in </a> </div>
 	<br><br><br><br>
+
 	<img class="pic_right" src="http://alumnimeet.iitkgp.ernet.in/images/Chinna%20Boddipali.jpg"width="100px" height="100px"><br>
 	<div class="name_right"><b>Mr. Chinna Babu Boddipalli (AE) Managing Director- Institutional Development,</b>  </div><br>
 	<div class="name_right"><b>Email:</b> <a href="mailto:chinna@adm.iitkgp.ernet.in" target="a_blank">chinna@adm.iitkgp.ernet.in</a></div>
 	<br><br><br><br>
+
 	<img class="pic_left" src="http://alumnimeet.iitkgp.ernet.in/images/Shampa%20goswami.jpg" width="100px" height="100px "><br>
 	<div class="name_left"><b>Ms. Shampa Goswami, Technical Superintendent, Administrative In-charge - AA& IR,   </b> </div><br>
 	<div class="name_left"> <b>Email:</b><a href="mailto:shampa@hijli.iitkgp.ernet.in" target="a_blank"> shampa@hijli.iitkgp.ernet.in</a> </div>
 	<br><br><br><br>
+
 	<img class="pic_right" src="http://alumnimeet.iitkgp.ernet.in/images/Shreyoshi%20Ghosh.jpg"width="100px" height="100px"><br>
 	<div class="name_right"><b>Ms. Shreyoshi Ghosh, Corporate Communication Executive - Institutional Development</b></div><br>
 	<div class="name_right"><b>Email:</b><a href="mailto:shreyoshi@adm.iitkgp.ernet.in"> shreyoshi@adm.iitkgp.ernet.in</a>;Ph: +91 3222 277480</div>
 	<br><br><br><br>
+
 	<img class="pic_left" src="http://alumnimeet.iitkgp.ernet.in/images/Shruti%20gupta.jpg" width="100px" height="100px"><br>
 	<div class="name_left"><b>Ms. Shruti Gupta, Institutional Development Executive - Institutional Development</b> </div><br>
 	<div class="name_left"><b>Email:</b><a href=" mailto:shruti@adm.iitkgp.ernet.in" target="a_blank">shruti@adm.iitkgp.ernet.in </a>GSM: 9475371800 </div>
 	<br><br><br><br>
-	<img class="pic_right" src="http://alumnimeet.iitkgp.ernet.in/images/aditi%20sharma.jpg"width="100px" height="100px"><br>
-	<div class="name_right"><b>Ms. Aditi Sharma, General Secretary, Students' Alumni Cell</b></div><br>
-	<div class="name_right"><b>Email:</b><a href="mailto:aditiritu.sharma@gmail.com" target="a_blank"> aditiritu.sharma@gmail.com</a>; GSM: +917407659789</div>
+
+	<img class="pic_right" src="" alt="picture" width="100px" height="100px"><br>
+	<div class="name_right"><b>Mr. Arpit Gupta, General Secretary, Students' Alumni Cell</b></div><br>
+	<div class="name_right"><b>Email:</b><a href="mailto:arpitgupta366@gmail.com" target="a_blank"> arpitgupta366@gmail.com</a>; GSM: +918101119095</div>
 	<br><br><br><br>
-	<img class="pic_left" src="http://alumnimeet.iitkgp.ernet.in/images/vedang.jpg" width="100px" height="100px"><br>
-	<div class="name_left"><b>Mr. Vedang Deshpande, General Secretary, Students' Alumni Cell</b> </div><br>
-	<div class="name_left"> <b>Email:</b><a href="mailto:vedang.28@gmail.com" target="a_blank"> vedang.28@gmail.com </a> ; GSM: +918001618061</div>
+
+	<img class="pic_left" src="" alt="picture" width="100px" height="100px"><br>
+	<div class="name_left"><b>Mr. Manish Goyal, General Secretary, Students' Alumni Cell</b> </div><br>
+	<div class="name_left"> <b>Email:</b><a href="mailto:gmanish1993@gmail.com" target="a_blank"> gmanish1993@gmail.com </a> ; GSM: +917797437023</div>
 	<br><br><br>
+
 	<br><br><br><br>
 	<div class="pic_left"> <b>Job Assistants:</b> Prasenjit Banerjee, Sadhan Banerjee</div><br>
 	<div class="pic_left"><b>Office Assistants:</b> Tapas Panda, Provas Kar</div>
