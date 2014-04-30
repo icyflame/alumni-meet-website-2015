31151684b7484b55a5017bb39c59f9aa040111ca
icyflame <kannan.siddharth12@gmail.com>
2014-04-30 12:58:56 +0530

    The last meet page was a copy of the events page. Now the content was changed to include the 10th Alum Meet content

--- content_lastmeet.php
+++ content_lastmeet.php
@@ -1,93 +1,162 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-<div id="content">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta content=
+    "HTML Tidy for Linux/x86 (vers 25 March 2009), see www.w3.org" name=
+    "generator" />
 
-  <p>
-    <div class="font_1">
-      <h3>There are a number of events planned for the three days of the Meet. It will be a perfect blend of nostalgia, fun, professionalism and entertainment.
-</h3>
-    </div>
-  </p>
-  
+    <title></title>
+</head>
 
-  <font size="4px"></font>
-   <div class="header_1">1.Inauguration Ceremony: 
-  </div>  <span style="font-size:14px">
-    A formal way to get started and welcome our alumni back to the campus. This auspicious occasion, with the invocation ceremony by the hands of the Director, marks the beginning of the three-day long event. There will be documentaries premiered on KGP to help you recapitulate of what had happened and what is happening in your college.
-  </span>
-  
-  <p><img src="events_images/inaguration.jpg"  width="40%"></p>
-
-  <div id="iphonePerspective" data-position="right" data-offset="250" class="notViewed animBlock">
-    <p><font size="4px" ></font> <div class="header_1">2.Campus Tour: </span>        <span style="font-size:14px">The whole batch will be taken around the campus with our volunteers and will be updated about the current developments in the campus. You will be also visiting your departments and the old beauties of the campus.
-    </div>
-  </p></div>
-  <div id="iphonePerspective" data-position="left-image" data-offset="250" class="notViewed animBlock">
-  <span style="float:right"><img src="events_images/campus_tour2.jpg"  width="40%"></span>
-  <!-- <img src="events_images/campus_tour1.jpg" width="750px" height="500px"> include this pic as the size of the image is reduced-->
-
-</div>
-<div id="iphonePerspective" data-position="left" data-offset="250" class="notViewed animBlock">
-  <p><font size="4px"></font> <div class="header_1">3.Hall Visits:  </span> <span style="font-size:14px">A night is solely dedicated to your hall- where you can revisit your room and relive the best times of your college life. The halls would be organising events for their respective alumni and after that its all yours....
-  </div>   
-</p>
-<img src="events_images/hall_visit.jpg"  width="40%">
-</div>
-<div id="iphonePerspective" data-position="right" data-offset="250" class="notViewed animBlock">
-  <p><font size="4px"></font> <div class="header_1">4.Musical Night:  </div>  
-  <span style="font-size:14px"> The mood transcends all senses as you go Gaga over Raga. A musical bonanza, where live performances shall floor you with their intoxicating blend of music and sounds, and perhaps get you closer to musical nirvana. So get ready to sway to some mesmerizing performances by the musicians of Kgp. Previous meets have set a precedent where mesmerised alumni take to the dais.
-  </span>                          
-</p></div>
-  <div id="iphonePerspective" data-position="left-image" data-offset="250" class="notViewed animBlock">
-
-<span style="float:right"><img src="events_images/musical_night.jpg"  width="40%"></span>
-</div>
-         <div id="iphonePerspective" data-position="left" data-offset="250" class="notViewed animBlock">
-           <p><font size="4px"></font> <div class="header_1">5.Fine Art and Photography Exhibition:</div> 
-		   <span style="font-size:14px ;width:100%">  Check out the skills of the students of IIT Kharagpur as they prepare paintings, sketches, models and click photographs for their dear alumni.The works will be put up for sale.  
-		   </span>      
-         </p>
-        <img src="events_images/fine_arts.jpg"  width="40%">
-       </div>
-
-       <div id="iphonePerspective" data-position="right" data-offset="250" class="notViewed animBlock">
-        <p><font size="4px"></font> <div class="header_1">6.Illumination and DJ night:    
+<body>
+    <div id="content">
+        <div class="font_1">
+            <h3>The 10th Alumni Meet was organised from 4th January to 6th
+            January, 2013.</h3>
         </div>
-        <span style="font-size:14px">A mini-illu made by our team for our guests. You will be lighting the lamps and experiencing the innovation Kgp has had. There will be a DJ playing for your feet to tap and mind to rock.
-        </span>
-      </p></div>  <div id="iphonePerspective" data-position="left-image" data-offset="250" class="notViewed animBlock">
 
-     <span > <img src="events_images/bonfire.jpg" width="40%"></span>
-      
+        <div class="header_1">
+            1. Departmental Visits:
+        </div><span style="font-size:14px">The alumni visited their respective
+        departments where they were briefed about the recent developments and
+        were enlightened about the department's vision for the future.</span>
+
+        <p><img src="events_images/inaguration.jpg" width="40%" /></p>
+
+        <div class="notViewed animBlock" data-offset="250" data-position=
+        "right" id="iphonePerspective">
+            <div class="header_1">
+                2. Musical Night: <span style="font-size:14px">An hour long
+                musical production by ETMS &amp; WTMS enjoyed by both alumni
+                and students. It was delightful to see many of our beloved
+                alumni turning up on stage to showcase their musical
+                talents.</span>
+            </div><span style="font-size:14px"><br />
+            <br /></span>
+        </div>
+
+        <div class="notViewed animBlock" data-offset="250" data-position="left"
+        id="iphonePerspective">
+            <div class="header_1">
+                3. Hall Visits: <span style="font-size:14px">This is one
+                segment of the Meet that is really close to the hearts of the
+                alumni. The alumni were at a loss to express the ecstasy and
+                emotions that welled up within them when they paid a visit to
+                their hallowed Halls of Residence. The students of the
+                respective halls had also organized various events for the
+                alumni.</span>
+            </div><span style="font-size:14px"><br />
+            <br />
+            <img src="events_images/hall_visit.jpg" width="40%" /></span>
+        </div>
+
+        <div class="notViewed animBlock" data-offset="250" data-position=
+        "right" id="iphonePerspective">
+            <div class="header_1">
+                <span style="font-size:14px">4. Inauguration Ceremony:</span>
+            </div><span style="font-size:14px"><span style="font-size:14px">The
+            formal inauguration of the 10th Annual Alumni Meet was presided
+            over by the Director, followed by memento distribution to the
+            alumni attending the Meet.</span><br />
+            <br /></span>
+        </div>
+
+        <div class="notViewed animBlock" data-offset="250" data-position=
+        "left-image" id="iphonePerspective">
+            <span style="float:right"><img src=
+            "events_images/musical_night.jpg" width="40%" /></span>
+        </div>
+
+        <div class="notViewed animBlock" data-offset="250" data-position="left"
+        id="iphonePerspective">
+        <div class="header_1">
+            5. Painting Exhibition:
+        </div><span style="font-size:14px ;width:100%">One of the visiting
+        alumni, Mr. Sridhar Ramasamy organized an exhibition of his painted
+        works which were auctioned during the Meet.</span> <img src=
+        "events_images/fine_arts.jpg" width="40%" /></div>
+
+        <div class="notViewed animBlock" data-offset="250" data-position=
+        "right" id="iphonePerspective">
+            <div class="header_1">
+                6. Entertainia:
+            </div><span style="font-size:14px">This edition of Entertania
+            witnessed the alumni of IIT Kharagpur performing for the alumni of
+            IIT Kharagpur. Yes, you heard that right. Theatre enthusiasts
+            Jitendra Kumar and Biswapati Sarkar (Batch of 2012) staged a
+            light-hearted romantic comedy titled “Happily Ever After”- A Sort
+            of Sequel to Every Love Story.</span>
+        </div>
+
+        <div class="notViewed animBlock" data-offset="250" data-position=
+        "left-image" id="iphonePerspective">
+            <span><img src="events_images/bonfire.jpg" width="40%" /></span>
+        </div>
+
+        <div class="notViewed animBlock" data-offset="250" data-position="left"
+        id="iphonePerspective">
+            <div class="header_1">
+                7. Illumination, DJ &amp; Bonfire: <span style=
+                "font-size:14px">The alumni lit up diyas and celebrated
+                illumination in KGP style. Later the alumni lightened a few
+                muscles by stepping to the beats of a DJ around the bonfire and
+                the funfilled night seemed to be everlasting.</span>
+            </div><span style="font-size:14px"><br />
+            <br />
+            <img src="events_images/2.2.jpg" width="40%" /></span>
+        </div>
+
+        <div class="notViewed animBlock" data-offset="250" data-position=
+        "right" id="iphonePerspective">
+            <div class="header_1">
+                <span style="font-size:14px">8. 2.2 Marathon:</span>
+                <span style="font-size:14px">A 2.2 km marathon was organised
+                which received an overwhelming participation from the alumni
+                and students. The winners were awarded trophies and ITC dinner
+                coupons.</span>
+            </div><span style="font-size:14px"><br />
+            <br /></span>
+        </div>
+
+        <div class="notViewed animBlock" data-offset="250" data-position=
+        "left-image" id="iphonePerspective">
+            <span style="float: right; font-size: 14px"><img src=
+            "events_images/entertania2.jpg" width="40%" /></span>
+        </div>
+
+        <div class="notViewed animBlock" data-offset="250" data-position="left"
+        id="iphonePerspective">
+            <div class="header_1">
+                9. BSE Seminar: <span style="font-size:14px">A seminar was
+                conducted to enlighten the students about Indian Capital
+                Markets. There were three panelists who with the help of their
+                experience and knowledge presented their views and discussed it
+                with the students. There was a moderator who moderated the
+                panel discussion and raised some relevant questions about the
+                topic. The seminar ended with a session involving doubts from
+                the students and conclusion from the moderator.</span>
+            </div><span style="font-size:14px"><br />
+            <br />
+            <img src="events_images/closing_ceremony.jpg" width="40%" /></span>
+        </div>
+
+        <div class="notViewed animBlock" data-offset="250" data-position="left"
+        id="iphonePerspective">
+            <div class="header_1">
+                9. Closing Ceremony: <span style="font-size:14px">A cultural
+                dance program themed on “Life at IIT Kgp” was organized by the
+                students for the alumni. There was also a special awards
+                distribution ceremony for the visiting alumni. A watch
+                sponsored by our official Time Keeper “Antonio Bernini” was
+                also auctioned during the Closing Ceremony.</span>
+            </div><span style="font-size:14px"><br />
+            <br />
+            <img src="events_images/closing_ceremony.jpg" width="40%" /></span>
+        </div>
+        
     </div>
-
-    <div id="iphonePerspective" data-position="left" data-offset="250" class="notViewed animBlock">
-      <p><font size="4px"></font> <div class="header_1">7.Sports Event:     </span> <span style="font-size:14px">Here is a chance for you to stretch your muscles again and carry pictures back home for your children with a note- "Son I am equally good!". Do send us suggestions for your preference of events.
-      </div>  
-    </p>
-    <img src="events_images/2.2.jpg"  width="40%">
-  </div>
-  <div id="iphonePerspective" data-position="right" data-offset="250" class="notViewed animBlock">
-    <p><font size="4px"></font> <div class="header_1">8.Entertainia:    </span>   <span style="font-size:14px">It is going to be a surprise! But surely you will love what is going to happen.
-    </div>
-  </p></div>
-    <div id="iphonePerspective" data-position="left-image" data-offset="250" class="notViewed animBlock">
-
-  <span style="float:right"><img src="events_images/entertania2.jpg"  width="40%"></span>
-
-</div>
-<div id="iphonePerspective" data-position="left" data-offset="250" class="notViewed animBlock">
-  <p><font size="4px"></font> <div class="header_1">9.Closing Ceremony: </span>       
-    <span style="font-size:14px"> The time to say Good Bye! We will leave you with memories in form of mementos and group photographs and some more nostalgia to carry back home. A special awards ceremony and a cultural program is also in store for the alumni.
-    </div>                              
-  </p>
- <img src="events_images/closing_ceremony.jpg"  width="40%">
-</div>
-
-
-
-
-</div>
-
-<br>
-<hr>
-<br>
+    <hr />
+</body>
+</html>
